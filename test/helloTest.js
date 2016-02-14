1c239c4e52966da08438b93b36026e8c52933a20
Christopher Clarke <chris@awesome.home>
2016-02-14 00:04:57 +0000

    Adding basic libs and test setup

---
+++ test/helloTest.js
@@ -0,0 +1,6 @@
+var assert = require('chai').assert;
+  describe('basic test setup', function () {
+    it('should run this test', function () {
+      assert.ok(true); // best test ever
+    });
+  });
